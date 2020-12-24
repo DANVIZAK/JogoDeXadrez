9b851687aa8c35ac112d7eb39e1b8074e487d14e
DANVIZAK <75839887+DANVIZAK@users.noreply.github.com>
2020-12-24 14:01:18 -0300

    Criacao da Classe Posicao

--- JogoXadrez/src/Program.java
+++ JogoXadrez/src/Program.java
@@ -1,8 +1,16 @@
 
+import Tabuleiro.Posicao;
+
+
 public class Program {
 
     
     public static void main(String[] args) {
+        
+        Posicao teste = new Posicao(1,2);
+        
+        System.out.println(teste);
+        
       
     }
     
